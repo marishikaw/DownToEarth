05d542f65205925f4acb3dd03b920cf640203427
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-05 07:47:45 +0000

    [Update]フラッシュメッセージのレイアウトを実装

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,52 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = '更新しました'
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @user = current_customer
+  end
+
+  def withdraw
+    user = current_customer
+    if user.update(is_deleted: true)
+      reset_session
+      redirect_to root_path
+    end      
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :icon_id)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/edit'
+  get 'user/show'
+  get 'user/unsubscribe'
+  get 'index/edit'
+  get 'index/show'
+  get 'index/update'
+  get 'index/unsubscribe'
+  get 'index/withdraw'
   devise_for :users, skip: 'passwords'
   
   root to: 'homes#top'
