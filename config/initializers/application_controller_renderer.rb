86cd69bbd3a0f160a6c0245981ea0313c8f58caf
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-06 02:31:48 +0000

    [Add]いいね機能とコメント機能を実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# refileシークレットキー
+Refile.secret_key = '3e917f953b0566c285c4ca6d4adb33b0acab90a582171721a6510357aec730d87a32a87bf5c095153817a83f62bc97940aaa3a087cde032d115d1d7785e108de'
