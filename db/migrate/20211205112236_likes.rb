0d69eee235418b995fd521e299ea7e9e9702659c
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-07 06:14:32 +0000

    [Update]不要なマイグレーションファイルを削除

--- db/migrate/20211205112236_likes.rb
+++
@@ -1,5 +0,0 @@
-class Likes < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :likes
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_112236) do
+ActiveRecord::Schema.define(version: 2021_12_05_111446) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
