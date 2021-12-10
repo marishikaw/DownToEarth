7e7d58c50daa788fa099bcdd761dd0d445ad4c89
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-10 12:43:34 +0000

    [Fix]コメントの非同期通信の不具合を修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
-    @comment.post_id = @post.id
     @comment.user_id = current_user.id
+    @comment.post_id = @post.id
     unless @comment.save
       render 'error'
     end
