febaac172c337480350924314be727d4fe2383ae
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-14 10:51:32 +0000

    [Add] Seed.rb Data

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,5 +1,17 @@
 puts 'posts ...'
 
-User.limit(20).each do |user|
-  user.posts.create!(caption: Faker::Quotes::Shakespeare.hamlet_quote)
+captions = [
+    'I will speak daggers to her but use none.',
+    'Do you think I am easier to be played on than a pipe?',
+    'The lady doth protest too much, methinks.',
+    'To be, or not to be, that is the question',
+    'Rich gifts wax poor when givers prove unkind.',
+    'Do you think I am easier to be played on than a pipe?',
+    'There is nothing either good or bad, but thinking makes it so.',
+    'To be, or not to be, that is the question',
+    'Doubt thou the stars are fire, doubt that the sun doth move, doubt truth to be a liar, but never doubt I love.',
+  ]
+
+User.all.each do |user|
+  user.posts.create!(caption: captions.sample)
 end
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,9 +1,27 @@
 puts 'users ...'
 
-15.times do |n|
+users = [
+    {email: '1@example.com', name: 'Lonnie', password: 'aaaaaa'},
+    {email: '2@example.com', name: 'Becky', password: 'aaaaaa'},
+    {email: '3@example.com', name: 'Dustin', password: 'aaaaaa'},
+    {email: '4@example.com', name: 'Steve', password: 'aaaaaa'},
+    {email: '5@example.com', name: 'Eleven', password: 'aaaaaa'},
+    {email: '6@example.com', name: 'Will', password: 'aaaaaa'},
+    {email: '7@example.com', name: 'Joyce', password: 'aaaaaa'},
+    {email: '8@example.com', name: 'Jonathan', password: 'aaaaaa'},
+    {email: '9@example.com', name: 'Nancy', password: 'aaaaaa'},
+    {email: '10@example.com', name: 'Diane', password: 'aaaaaa'},
+    {email: '11@example.com', name: 'Jim', password: 'aaaaaa'},
+    {email: '12@example.com', name: 'Lucas', password: 'aaaaaa'},
+    {email: '13@example.com', name: 'Mike', password: 'aaaaaa'},
+    {email: '14@example.com', name: 'Terry', password: 'aaaaaa'},
+    {email: '15@example.com', name: 'Barbara', password: 'aaaaaa'},
+  ]
+
+users do |user|
   user = User.create!(
-    email: "test#{n + 1}@com",
-    name: Faker::TvShows::StrangerThings.unique.character,
-    password: 'aaaaaa'
+    email: user[:email],
+    name: user[:name],
+    password: user[:password]
   )
 end
