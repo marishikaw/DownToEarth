7c2fd1f10330c44a535108533baa9878f7f8df18
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-12-15 02:19:00 +0000

    [Fix]TOPに戻るボタンを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-
 // jquery3 => jqueryに変更（本番環境にてjscrollが動作しないため）
 //= require jquery
 //= require jquery.jscroll.min.js
@@ -42,14 +29,17 @@ $(window).scroll(
   function(){
     var now = $(window).scrollTop();
     if(now > 500){
-      $("#page-top").fadeIn("slow");
+      $('#page-top').fadeIn('slow');
     }else{
-      $("#page-top").fadeOut("slow");
+      $('#page-top').fadeOut('slow');
     }
   }
 );
-$("#move-page-top").click(
-  function(){
-    $("html,body").animate({scrollTop:0},"slow");
-  }
-);
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
