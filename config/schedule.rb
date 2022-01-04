be3df71bce7e438c514baaa54787163632a9c81a
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-01-04 09:57:45 +0000

    [Update]schedule.rbにコメントを追加

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,15 +19,23 @@
 
 # Learn more: http://github.com/javan/whenever
 
+# 絶対パスを取得
 require File.expand_path(File.dirname(__FILE__) + "/environment")
+
+# 現在の環境をシンボル型で取得
 rails_env = Rails.env.to_sym
+
 set :environment, rails_env
 set :output, 'log/cron.log'
+
 every 1.hours do
   begin
     runner "Batch::DataReset.data_reset"
+  # 例外処理　エラー内容を格納
   rescue => e
+    # ログに出力
     Rails.logger.error("aborted rails runner")
+    # エラーを明示的に発生させる
     raise e
   end
 end
